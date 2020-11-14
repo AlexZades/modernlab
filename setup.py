c4bbe4316a78ab511952d446778c45845a9dbae5
AlexZades <az@st4r.io>
2020-11-14 17:45:34 -0500

    Published Package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Azrules", # Replace with your own username
+    name="modernlab", 
     version="0.0.1",
     author="Alex Zades",
     author_email="az@st4r.io",
